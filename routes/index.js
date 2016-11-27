8ae14230a341cb5cf28633cd0ed02d06d7a6a549
Saad Ahmed <saadem3000@gmail.com>
2016-11-27 03:25:53 -0500

    Add download file for thesis dataset

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Saad Ahmed' });
 });
 
+/* GET download dataset file, for Master's Thesis */
+router.get('/datasets/parkmarcotte', function(req, res){
+  var file = './data/ParkMarcotte.tar.gz';
+  res.download(file); 
+});
+
 module.exports = router;
